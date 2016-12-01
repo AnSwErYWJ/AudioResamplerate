bd41b279fb2671dc42492ee28600640cfffbd667
answerywj <ywj1993@yeah.net>
2016-12-01 14:01:54 +0800

    clean .d .o

--- source/resamplerate/src_linear.d
+++
@@ -1,3 +0,0 @@
-resamplerate.o source/resamplerate/src_linear.d : source/resamplerate.c include/resamplerate/samplerate.h \
- include/iniparser/iniparser.h include/iniparser/dictionary.h \
- include/log/log.h include/resamplerate/resamplerate.h
